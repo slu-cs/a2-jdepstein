f2b84c680eae0c9b37203906641ffd86c0219730
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-02-20 11:34:13 -0500

    Schema and Create

---
+++ create.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const readline = require('readline');
+const mongoose = require('mongoose');
+const connect = require('./db');
+const Professor = require('./schema');
+
+connect(); // To the database
+
+const file = readline.createInterface({
+  input: fs.createReadStream('voters.csv')
+
+});
+
+const votes = [];
+counter = 0
+file.on('line', function(line) {
+  const columns = line.split(',');
+  votes.push({
+    const counter = new Voter({
+      first: columns[0],
+      last: columns[1],
+      zip: Number(columns[2]),
+      history:colums[2:]
+    });
+    counter += 1
+  });
+});
+
+// Let the popularity score for a discipline be the number of majors it has plus half the number of minors.
+// Sort disciplines by (increasing) popularity.
+
+const saves = rows.map(d => d.save());
+mongoose.connection.dropDatabase()
+    Promise.all(saves))
+    .then(() => console.log('All saved'))
+    .then(() => mongoose.connection.close())
+    .then(() => console.log('Database is ready.'))
+    .catch(error => console.log(error.stack));
---
+++ schema.js
@@ -0,0 +1,21 @@
+
+// Define a plan for a collection
+
+const mongoose = require('mongoose');
+
+// Schema for a collection of professors
+const Voter = new mongoose.Schema({
+first: String,
+last: String,
+zip: Number,
+history: [String]
+});
+
+// Speed up queries on all fields
+Voter.index({name: 1});
+Voter.index({rank: 1});
+Voter.index({started: 1});
+Voter.index({courses: 1});
+
+// Compile and export this schema
+module.exports = mongoose.model('Voter', Voter);
