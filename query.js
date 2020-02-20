0f9fbc472f556ca7e7c6e1c7bfb24d68fffd2bed
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-02-20 11:54:31 -0500

    Queries

---
+++ query.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const connect = require('./db');
+const Voter = require('./schema');
+
+connect(); // To the database
+
+/*// What documents are in the collection?
+const query = Professor.find();
+query.exec(function(error, professors) {
+  if (error) console.error(error.stack);
+  console.log(professors);
+});*/
+
+const queries = [
+  Voter.count(),
+
+  Voter.find().where('first').equals('STARR'),
+
+  Voter.find().where('history').in('GE16'),
+
+  Voter.find().distnct('zip').count(),
+
+  Voter.sort('last').find().where('zip').equals(13617).last()
+
+]
+
+Promise.all(queries)
+  .then(function(results) {
+    console.log('Registered voters: ', results[0]);
+    console.log('Voters with name STARR: ', results[1]);
+    console.log(' 2016 general election Voters: ', results[2]);
+    console.log('last-name that comes last in the county: ', results[3]);
+    console.log('Distinct zip codes: ', results[4]);
+    mongoose.connection.close();
+  }).catch(error => console.error(error.stack));
--- schema.js
+++ schema.js
@@ -5,11 +5,11 @@ const mongoose = require('mongoose');
 
 // Schema for a collection of professors
 const Voter = new mongoose.Schema({
-first: String,
-last: String,
-zip: Number,
-history: [String]
-});
+    first: String,
+    last: String,
+    zip: Number,
+    history: [String]
+    });
 
 // Speed up queries on all fields
 Voter.index({name: 1});
