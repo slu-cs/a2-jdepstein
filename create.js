08f44bec7fa9219f6f315b787220e83964aebbac
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-02-21 10:44:55 -0500

    edits

--- create.js
+++ create.js
@@ -16,7 +16,7 @@ counter = 0
 file.on('line', function(line) {
   const columns = line.split(',');
   votes.push({
-      new Voter({
+      const counter = new Voter({
       first: columns[0],
       last: columns[1],
       zip: Number(columns[2]),
