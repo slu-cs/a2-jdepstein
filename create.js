457209abfea29cced0de42e1ae87e8d891dd7468
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-02-21 11:07:48 -0500

    fixed

--- create.js
+++ create.js
@@ -12,26 +12,25 @@ const file = readline.createInterface({
 });
 
 const votes = [];
-counter = 0
 file.on('line', function(line) {
   const columns = line.split(',');
-  votes.push({
-        Voter({
+  votes.push(
+        new Voter({
           first: columns[0],
           last: columns[1],
           zip: Number(columns[2]),
           history: colums.slice(3)
     });
-    counter += 1
-  });
+
+  );
 });
 
 // Let the popularity score for a discipline be the number of majors it has plus half the number of minors.
 // Sort disciplines by (increasing) popularity.
 
-const saves = votes.map(d => d.save());
+
 mongoose.connection.dropDatabase()
-    Promise.all(saves))
+    .then(() => Promise.all(votes.map(d => d.save())))
     .then(() => mongoose.connection.close())
     .then(() => console.log('Database is ready.'))
     .catch(error => console.log(error.stack));
