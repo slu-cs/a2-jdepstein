0052ac218f6da95f400565d9564bd7ad07e1b574
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-02-23 18:33:34 -0500

    test

--- query.js
+++ query.js
@@ -42,6 +42,6 @@ Promise.all(queries)
     console.log('Number of 2016 general election Voters: ', results[2].length);
     console.log('last-name that comes last in the county in alphabetical order:', results[3].map(v => v.last));
     console.log('Number of Distinct zip codes: ', results[4].length);
-    console.log('Total Voters: ', results[5].length);
+    console.log('Total Voters: ', results[5]);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
