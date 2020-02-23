090761e9a09cc76d5a2b22d2d0abfa00586137d8
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-02-23 18:29:42 -0500

    test

--- query.js
+++ query.js
@@ -13,19 +13,19 @@ query.exec(function(error, professors) {
 
 const queries = [
   //1)
-  Voter.find().where('zip').equals('13617'),
+  //Voter.find().where('zip').equals('13617'),
 
   //2)
-  Voter.find().where('first').equals('STARR'),
+  //Voter.find().where('first').equals('STARR'),
 
   //3)
   Voter.find().where('history').in("GE16"),
 
   //4)
-  Voter.find().sort('-last').limit(1),
+  //Voter.find().sort('-last').limit(1),
 
   //5)
-  Voter.distinct('zip')
+  //Voter.distinct('zip')
 
 
 
@@ -34,9 +34,9 @@ const queries = [
 Promise.all(queries)
   .then(function(results) {
     console.log('Number of registered voters in Canton: ', results[0].length);
-    console.log('Voters with name the first name STARR: ', results[1].map(v => (v.first + " " + v.last)));
-    console.log('Number of 2016 general election Voters: ', results[2].length);
-    console.log('last-name that comes last in the county in alphabetical order:', results[3].map(v => v.last));
-    console.log('NUmber of Distinct zip codes: ', results[4].length);
+    //console.log('Voters with name the first name STARR: ', results[1].map(v => (v.first + " " + v.last)));
+  //  console.log('Number of 2016 general election Voters: ', results[2].length);
+  //  console.log('last-name that comes last in the county in alphabetical order:', results[3].map(v => v.last));
+  //  console.log('NUmber of Distinct zip codes: ', results[4].length);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
