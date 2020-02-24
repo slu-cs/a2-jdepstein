e6978cb30512b892aeee4dac32aed400f692749b
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-02-24 10:11:17 -0500

    ()

--- query.js
+++ query.js
@@ -14,7 +14,7 @@ const queries = [
   Voter.find().where('first').equals('STARR'),
 
   //3)
-  Voter.find().({"history":{$regex: '.*GE16.*'}}),
+  Voter.find({"history":{$regex: '.*GE16.*'}}),
 
   //4)
   Voter.find().sort('-last').limit(1),
